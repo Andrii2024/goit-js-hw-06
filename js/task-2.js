fb5723f87828e68e3f481ff2f5097333c9f415f8
Andrii2024 <krovlya2005@gmail.com>
2024-01-09 17:18:24 +0200

    6

--- js/task-2.js
+++ js/task-2.js
@@ -12,6 +12,25 @@
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і
 // встав його після оголошення класу для перевірки коректності роботи.
 //  У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = [...items];
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const indexToRemove = this.#items.indexOf(itemToRemove);
+    if (indexToRemove !== -1) {
+      this.#items.splice(indexToRemove, 1);
+    }
+  }
+}
+
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
